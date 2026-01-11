b2c794f457a298092c95c85747ec19a42345f802
Vivek Kumar <194188560+the-vivek-dev@users.noreply.github.com>
2026-01-11 14:40:43 +0530

    Refactor MultipleCheckbox to use selectedFruits state

--- src/MultipleCheckbox.jsx
+++ src/MultipleCheckbox.jsx
@@ -1,44 +1,80 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 const MultipleCheckbox = () => {
-    const [fruitsArray, setFruitsArray] = useState([]);
+  const [selectedFruits, setSelectedFruits] = useState([]);
 
-    const handleChange = (e) => {
-        const { value, checked } = e.target;
+  const handleChange = (e) => {
+    const { value, checked } = e.target;
 
-        setFruitsArray(prev =>
-            checked
-                ? [...prev, value]
-                : prev.filter(item => item !== value)
-        );
-    };
+    setSelectedFruits((prev) =>
+      checked
+        ? [...prev, value]
+        : prev.filter((item) => item !== value)
+    );
+  };
 
-    const handleSubmit = (e) => {
-        e.preventDefault();
-        console.log(fruitsArray)
-    }
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Selected fruits:", selectedFruits);
+  };
 
-    return (
-        <div>
-            <form onSubmit={handleSubmit}>
-                <label htmlFor="">Select Fruits: &nbsp;</label>
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <fieldset>
+          <legend>Select Fruits</legend>
 
-                <input type="checkbox" name='fruits' value="Apple" onChange={handleChange} />
-                <label htmlFor="">Apple</label>
-                &nbsp;
-                <input type="checkbox" name='fruits' value="Mango" onChange={handleChange} />
-                <label htmlFor="">Mango</label>
-                &nbsp;
-                <input type="checkbox" name='fruits' value="Orange" onChange={handleChange} />
-                <label htmlFor="">Orange</label>
-                &nbsp;
-                <input type="checkbox" name='fruits' value="Banana" onChange={handleChange} />
-                <label htmlFor="">Banana</label>
-                <br /><br />
-                <button type='submit'>Submit</button>
-            </form>
-        </div>
-    )
-}
+          <div>
+            <input
+              type="checkbox"
+              id="apple"
+              value="Apple"
+              checked={selectedFruits.includes("Apple")}
+              onChange={handleChange}
+            />
+            <label htmlFor="apple">Apple</label>
+          </div>
 
-export default MultipleCheckbox
+          <div>
+            <input
+              type="checkbox"
+              id="mango"
+              value="Mango"
+              checked={selectedFruits.includes("Mango")}
+              onChange={handleChange}
+            />
+            <label htmlFor="mango">Mango</label>
+          </div>
+
+          <div>
+            <input
+              type="checkbox"
+              id="orange"
+              value="Orange"
+              checked={selectedFruits.includes("Orange")}
+              onChange={handleChange}
+            />
+            <label htmlFor="orange">Orange</label>
+          </div>
+
+          <div>
+            <input
+              type="checkbox"
+              id="banana"
+              value="Banana"
+              checked={selectedFruits.includes("Banana")}
+              onChange={handleChange}
+            />
+            <label htmlFor="banana">Banana</label>
+          </div>
+
+        </fieldset>
+
+        <br />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
+  );
+};
+
+export default MultipleCheckbox;
